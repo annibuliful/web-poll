ba528f4132d991afb56762cd63d0ae90c9458451
lagmanzaza <lagmanmc@gmail.com>
2018-07-30 14:15:23 +0700

    [Component , Props , Service] poll Component , liking and unliking service

--- frontend/src/components/Card.vue
+++ frontend/src/components/Card.vue
@@ -1,11 +1,26 @@
 <template>
 <div class="card" style="width: 20rem;">
   <div class="card-body">
-    <h4 class="card-title">My awesome Paper card!</h4>
-    <h5 class="card-subtitle">Nice looking subtitle.</h5>
-    <p class="card-text">This is another example of a card without image. Cards are also meant to be used without images, but with text/links/buttons.</p>
-    <a class="card-link" href="#">First link</a>
-    <a class="card-link" href="#">Second link</a>
+    <h4 class="card-title">{{title}}</h4>
+    <p class="card-text">{{detail}}</p>
   </div>
 </div>
 </template>
+<script>
+  export default{
+    props:{
+      title:{
+        type: String,
+        required: true
+      },
+      detail:{
+        type: String,
+        required: true
+      },
+      pollId:{
+        type: String,
+        required: true
+      }
+    }
+  }
+</script>
